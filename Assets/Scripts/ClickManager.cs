e57a8707cb8ec605821ef41f392427c322271101
cowboydonkey777 <yggdrasil789@gmail.com>
2022-01-25 18:33:04 -0800

    First Commit

---
+++ Assets/Scripts/ClickManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickManager : MonoBehaviour
+{
+    public List<float> autoClickersLastTime = new List<float>();
+    public int autoClickerPrice;
+    public TextMeshProUGUI quantityText;
+
+    void Update()
+    {
+        for (int i = 0; i < autoClickersLastTime.Count; i++)
+        {
+            if(Time.time - autoClickersLastTime[i] >= 1.0f)
+            {
+                autoClickersLastTime[i] = Time.time;
+                EnemyManager.instance.curEnemy.Damage();
+            }
+        }
+    }
+
+    public void OnBuyAutoClicker()
+    {
+        if(GameManager.instance.gold >= autoClickerPrice)
+        {
+            GameManager.instance.TakeGold(autoClickerPrice);
+            autoClickersLastTime.Add(Time.time);
+
+            quantityText.text = "X " + autoClickersLastTime.Count.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+    public int curHp;
+    public int maxHp;
+    public int goldToGive;
+    public Image healthBarFill;
+    public Animation anim;
+
+    public void Damage()
+    {
+        curHp--;
+        healthBarFill.fillAmount = (float)curHp / (float)maxHp;
+
+        anim.Stop();
+        anim.Play();
+
+        if(curHp <= 0)
+        {
+            Defeated();
+        }
+    }
+
+    public void Defeated()
+    {
+        GameManager.instance.AddGold(goldToGive);
+        EnemyManager.instance.DefeatEnemy(gameObject);
+    }
+}
